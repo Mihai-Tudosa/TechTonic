8c04ba7953d4244fde987b368d07d041a26335bc
Mihai-Tudosa <mihaitudd@gmail.com>
2024-10-21 02:29:43 +0300

    local cart add function created, no html/css

--- js/details.js
+++ js/details.js
@@ -1,19 +1,16 @@
-//MockAPI Database
 const URL = `https://670fe588a85f4164ef2c6118.mockapi.io/products`;
 
-//URL query parameter to variable
-
+// URL query parameter to variable
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const queryID = urlParams.get("id");
 
-//Onload Function
+// Onload Function
 window.onload = function () {
   showProducts(queryID);
 };
 
-//Load Product
-
+// Load Product
 function showProducts(id) {
   fetch(`${URL}/${id}`)
     .then((response) => response.json())
@@ -21,19 +18,37 @@ function showProducts(id) {
       document.getElementById("item-container").innerHTML = `
         <div class="big-image product-card details-card"><img src=${product.imageURL} /></div>
         <div class="single-item-details product-card details-card">
-            <div class = "name">${product.name}</div>
-            <div class = "details">${product.details}</div>
-            <div class = "price">Preț: ${product.price} RON</div>
-            <div class = "quantity">În stoc: ${product.quantity} bucăți.</div>
-            <div class = "qty-select">
+          <div class="name">${product.name}</div>
+          <div class="details">${product.details}</div>
+          <div class="price">Preț: ${product.price} RON</div>
+          <div class="quantity">În stoc: ${product.quantity} bucăți.</div>
+          <div class="qty-select">
             <label for="quantity">Cantitate:</label>
             <input type="number" id="quantity" name="quantity" value="1">
-            </div>
-            <div id="button-add-to-cart" class ="cart-add"><span class="material-symbols-outlined">
-add_shopping_cart
-</span>Adaugă în coș</div>
-            
+          </div>
+          <div id="button-add-to-cart" class="cart-add"><span class="material-symbols-outlined">add_shopping_cart</span>Adaugă în coș</div>
         </div>
       `;
+      const addCartButton = document.getElementById("button-add-to-cart");
+      addCartButton.addEventListener("click", () =>
+        addToCart(queryID, document.getElementById("quantity").value)
+      );
     });
+}
+
+function addToCart(itemId, quantity) {
+  let cart;
+  if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+  } else {
+    cart = [];
+  }
+  const existingItem = cart.find((item) => item.id === itemId);
+  if (existingItem) {
+    existingItem.quantity += quantity;
+  } else {
+    cart.push({ id: itemId, quantity: quantity });
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+  console.log(localStorage.getItem("cart"));
 }

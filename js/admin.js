8a00784b6af79e3fe06cc3ea7cf7a20921be9944
Mihai-Tudosa <mihaitudd@gmail.com>
2024-10-20 06:05:58 +0300

    facut buton de edit functional

--- js/admin.js
+++ js/admin.js
@@ -53,10 +53,10 @@ function getSortProducts(e) {
           (product) => `
           <div class = "line-item product-line grid-ten-column">
             <img class = "line-item product-image grid-one-column" src=${product.imageURL} />
-            <h3 class = "line-item name grid-six-column" >${product.name}</h3>
+            <h3 class = "line-item name grid-six-column" ><a href= "#" onclick = "editProduct(${product.id})">${product.name}</a></h3>
             <p class="line-item price grid-one-column">${product.price} RON</p>
             <p class="line-item quantity grid-one-column">${product.quantity}</p>
-              <div class="line-item trash-can grid-one-column"><a href="details.html"><span class="material-symbols-outlined">
+              <div class="line-item trash-can grid-one-column"><a href= "#" onclick = "deleteProduct(${product.id})"><span class="material-symbols-outlined">
 delete
 </span></a></div>
         </div>
@@ -64,6 +64,22 @@ delete
         )
         .join("")}`;
     });
+}
+
+//DeleteProduct Function
+
+function deleteProduct(id) {
+  fetch(`${URL}/${id}`, { method: "DELETE" })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network Error`);
+      }
+      console.log(`Deleted product with ID ${id}`);
+      getSortProducts({ target: { value: "noi" } });
+    })
+    .catch((error) =>
+      console.error(`There was an error with the fetch operation:`, error)
+    );
 }
 
 //AddProduct Event Listener
@@ -119,9 +135,93 @@ function showAddProductForm() {
   }
 }
 
+//Function To Edit Product
+
+function editProduct(id) {
+  fetch(`${URL}/${id}`)
+    .then((response) => response.json())
+    .then((product) => {
+      document.getElementById("content-table").innerHTML = `
+
+  <div></div>
+  <label for="image-url">Image URL</label>
+  <div></div>
+  <input type="text" id="image-url" name="image-url" value="${
+    product.imageURL
+  }">
+  <div></div>
+
+  <div></div>
+  <label for="name">Name</label>
+  <div></div>
+  <input type="text" id="name" name="name" value="${product.name}">
+  <div></div>
+
+  <div></div>
+  <label for="description">Description</label>
+  <div></div>
+  <input type="text" id="description" name="description"  value="${
+    product.details
+  }">
+  <div></div>
+
+  <div></div>
+  <label for="price">Price</label>
+  <div></div>
+  <input type="number" id="price" name="price"  value="${Number(
+    product.price
+  )}">
+  <div></div>
+
+  <div></div>
+  <label for="quantity">Quantity</label>
+  <div></div>
+  <input type="number" id="quantity" name="quantity"  value="${Number(
+    product.quantity
+  )}">
+  <div></div>
+  
+  <div class="grid-filler"></div>
+  <div id="button-update-product">Modifică</div>
+  <div class="grid-filler"></div>
+  <div class="grid-filler"></div>
+  <div id="button-cancel">Anulare</div>
+  <div class="grid-filler"></div>
+  `;
+      const updateProductButton = document.getElementById(
+        "button-update-product"
+      );
+      if (updateProductButton) {
+        updateProductButton.addEventListener("click", () =>
+          updateProductInDatabase(id)
+        );
+      }
+    })
+    .catch((error) => console.error(`Error:`, error));
+
+  function updateProductInDatabase(id) {
+    const updatedProduct = {
+      name: document.getElementById(`name`).value,
+      details: document.getElementById(`description`).value,
+      price: document.getElementById(`price`).value,
+      quantity: document.getElementById(`quantity`).value,
+      imageURL: document.getElementById(`image-url`).value,
+    };
+    fetch(`${URL}/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedProduct),
+    })
+      .then((response) => response.json())
+      .then((updatedProduct) => {
+        console.log(`Updated entry`, updatedProduct);
+        getSortProducts({ target: { value: "noi" } });
+      })
+      .catch((error) => console.error(`Error:`, error));
+  }
+}
+
 //Post New Product (Save) Event Listener
-// const postNewProduct = document.getElementById(`button-add-product`);
-// postNewProduct.addEventListener(`click`, addNewProductInDatabase);
 
 function addNewProductInDatabase() {
   const newProduct = {
@@ -138,6 +238,9 @@ function addNewProductInDatabase() {
     body: JSON.stringify(newProduct),
   })
     .then((response) => response.json())
-    .then((newProduct) => console.log(`Succesful entry`, newProduct))
+    .then((newProduct) => {
+      console.log(`Successful entry`, newProduct);
+      getSortProducts({ target: { value: "noi" } });
+    })
     .catch((error) => console.error(`Error:`, error));
 }
